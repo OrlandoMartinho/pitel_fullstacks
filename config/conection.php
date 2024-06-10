0d0bbd0b7f1b184ca67531842494bfb91b7e92d9
Orlando Martinho <srsaiombo@gmail.com>
2024-06-10 09:21:38 +0100

    finalizando frontend

---
+++ config/conection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";  // Servidor do banco de dados
+$username = "root";         // Nome de usuário do banco de dados
+$password = "";             // Senha do banco de dados
+$dbname = "nome_do_banco";  // Nome do banco de dados
+
+// Criar a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+echo "Conexão bem-sucedida!";
+?>
--- index.php
+++ index.php
@@ -3,20 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pitel</title>
+    <title>Pitéu</title>
     <link rel="stylesheet" href="public/css/style.css">
     <link rel="stylesheet" href="public/css/font.css">
     <link rel="shortcut icon" href="public/assets/icon/logo.svg" type="image/x-icon">
 </head>
 <body>
     <div class="loader-container">
-       
         <img src="public/assets/img/loader.gif" alt="Loader" class="loader">
-      </div>
+    </div>
     <header>
         <nav>
-            <a href="" class="logo">
-                <img src="public/assets/icon/logo.svg" alt="">
+            <a href="#" class="logo">
+                <img src="public/assets/icon/logo.svg" alt="Logo">
             </a>
             <ul>
                 <li><a href="#">Home</a></li>
@@ -24,75 +23,70 @@
                 <li><a href="#destaque">Destaque</a></li>
                 <li><a href="#reserva">Reserva</a></li>
             </ul>
-          
-                <ul class="background-btn" style="cursor: pointer;">
-                    <a href="#contact" class="btn-header" >Contactar-nos</a>
-                </ul>
+            <ul class="background-btn" style="cursor: pointer;">
+                <a href="#contact" class="btn-header">Contactar-nos</a>
+            </ul>
         </nav>
     </header>
     <div class="hero">
         <div class="information-hero">
             <h1>Faça a sua reserva:</h1>
-            <h2>Sabores Únicos Esperam <br> por Você No <span>Pitéu </span>!</h2>
+            <h2>Sabores Únicos Esperam <br> por Você No <span>Pitéu</span>!</h2>
             <div class="cards">
                 <div class="card">
-                    <img src="public/assets/icon/icons8_vegan_food.png" alt="">
-                    <p>Culinária  Autêntica</p>
+                    <img src="public/assets/icon/icons8_vegan_food.png" alt="Culinária Autêntica">
+                    <p>Culinária Autêntica</p>
                 </div>
                 <div class="card">
-                    <img src="public/assets/icon/Grupo de máscara 1.png" alt="">
+                    <img src="public/assets/icon/Grupo de máscara 1.png" alt="Ambiente Acolhedor">
                     <p>Ambiente Acolhedor</p>
                 </div>
                 <div class="card">
-                    <img src="public/assets/icon/Grupo de máscara 2.png" alt="">
+                    <img src="public/assets/icon/Grupo de máscara 2.png" alt="Serviço de Qualidade">
                     <p>Serviço de Qualidade</p>
                 </div>
             </div>
         </div>
-
         <span class="onda">
-            <img src="public/assets/icon/icons8_wave_lines.png" alt="">
+            <img src="public/assets/icon/icons8_wave_lines.png" alt="Onda">
         </span>
-
         <span class="elipse1">
-            <img src="public/assets/img/Elipse 2.png" alt="">
+            <img src="public/assets/img/Elipse 2.png" alt="Elipse 1">
         </span>
-
         <span class="elipse2">
-            <img src="public/assets/img/Elipse 3.png" alt="">
+            <img src="public/assets/img/Elipse 3.png" alt="Elipse 2">
         </span>
-
         <span class="line">
-            <img src="public/assets/img/line.png" alt="">
+            <img src="public/assets/img/line.png" alt="Linha">
         </span>
         <div class="information-img">
-            <img src="public/assets/img/Grupo 76.png" alt="">
+            <img src="public/assets/img/Grupo 76.png" alt="Informação">
         </div>
     </div>
     <div class="about" id="about">
         <div class="about-img">
-            <img src="public/assets/img/aboutImg.png" alt="">
+            <img src="public/assets/img/aboutImg.png" alt="Sobre nós">
         </div>
         <div class="about-information">
             <div class="about-content">
                 <h3>Sobre nós</h3>
                 <p>Experimente a culinária angolana autêntica no Pitéu. <br>
-                    Reserve sua mesa agora para uma experiência <br>
-                    gastronômica única e memorável.
+                   Reserve sua mesa agora para uma experiência <br>
+                   gastronômica única e memorável.
                 </p>
             </div>
             <div class="about-content">
                 <h3>Sobre nós</h3>
                 <p>Experimente a culinária angolana autêntica no Pitéu. <br>
-                    Reserve sua mesa agora para uma experiência <br>
-                    gastronômica única e memorável.
+                   Reserve sua mesa agora para uma experiência <br>
+                   gastronômica única e memorável.
                 </p>
             </div>
             <div class="about-content">
                 <h3>Sobre nós</h3>
                 <p>Experimente a culinária angolana autêntica no Pitéu. <br>
-                    Reserve sua mesa agora para uma experiência <br>
-                    gastronômica única e memorável.
+                   Reserve sua mesa agora para uma experiência <br>
+                   gastronômica única e memorável.
                 </p>
             </div>
         </div>
@@ -101,74 +95,74 @@
         <h3>Destaque</h3>
         <div class="main-cards">
             <div class="main-card">
-                <img src="public/assets/img/prato01.png" alt="">
+                <img src="public/assets/img/prato01.png" alt="Arroz com carne">
                 <p>Arroz com carne</p>
             </div>
             <div class="main-card">
-                <img src="public/assets/img/prato02.png" alt="">
+                <img src="public/assets/img/prato02.png" alt="Arroz com feijão">
                 <p>Arroz com feijão</p>
             </div>
             <div class="main-card">
-                <img src="public/assets/img/prato01.png" alt="">
+                <img src="public/assets/img/prato01.png" alt="Arroz com batatas fritas">
                 <p>Arroz com batatas fritas</p>
             </div>
             <div class="main-card">
-                <img src="public/assets/img/prato03.png" alt="">
+                <img src="public/assets/img/prato03.png" alt="Arroz com feijão">
                 <p>Arroz com feijão</p>
             </div>
         </div>
     </div>
-    <div class="Reserva" id="reserva">
+    <div class="reserva" id="reserva">
         <h3>Reserva</h3>
         <div class="reservas-information">
             <div class="content-reservas">
-                <img src="public/assets/img/image-fruits.png" alt="">
-                <p>Garanta seu lugar e saboreie a autêntica culinária  no Pitéu! <br>
-                    Após reservar uma mesa ,ela expirará depois de 10 min do <br>
-                    horário definido!</p>
+                <img src="public/assets/img/image-fruits.png" alt="Reservas">
+                <p>Garanta seu lugar e saboreie a autêntica culinária no Pitéu! <br>
+                   Após reservar uma mesa, ela expirará depois de 10 min do <br>
+                   horário definido!</p>
             </div>
-            <div class="form">
+            <form class="form">
                 <div class="input-content">
-                    <label for="">Nome Completo</label>
-                    <input type="text" name="" id="">
+                    <label for="nome">Nome Completo</label>
+                    <input type="text" id="nome" name="nome">
                 </div>
                 <div class="input-content">
-                    <label for="">Data</label>
-                    <input type="date" name="" id="">
+                    <label for="data">Data</label>
+                    <input type="date" id="data" name="data">
                 </div>
                 <div class="input-content">
-                    <label for="">Hora</label>
-                    <input type="time" name="" id="">
+                    <label for="hora">Hora</label>
+                    <input type="time" id="hora" name="hora">
                 </div>
                 <div class="input-content">
-                    <label for="">Total de pessoas</label>
-                    <input type="number" name="" id="">
+                    <label for="pessoas">Total de pessoas</label>
+                    <input type="number" id="pessoas" name="pessoas">
                 </div>
                 <div class="input-content">
-                    <label for="">Email</label>
-                    <input type="email" name="" id="">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email">
                 </div>
-                <button class="btn">Reservar</button>
-            </div>
+                <input class="btn" type="submit" value="Reservar">
+            </form>
         </div>
     </div>
     <div class="contacts" id="contact">
-        <h3 id="contact">Contactos</h3 >
+        <h3>Contactos</h3>
         <div class="contacts-form">
             <form action="">
                 <div class="contacts-input">
-                    <label for="">Nome:</label>
-                    <input type="text" name="" id="">
+                    <label for="contact-nome">Nome:</label>
+                    <input type="text" id="contact-nome" name="contact-nome">
                 </div>
                 <div class="contacts-input">
-                    <label for="">Email:</label>
-                    <input type="email" name="" id="">
+                    <label for="contact-email">Email:</label>
+                    <input type="email" id="contact-email" name="contact-email">
                 </div>
                 <div class="contacts-input description">
-                    <label for="">Mensagem</label>
-                    <textarea name="" id="" cols="30"></textarea>
+                    <label for="mensagem">Mensagem</label>
+                    <textarea id="mensagem" name="mensagem" cols="30" rows="10"></textarea>
                 </div>
-                <button class="btn-contacts">Enviar</button>
+                <button class="btn-contacts" type="submit">Enviar</button>
             </form>
         </div>
     </div>
@@ -177,15 +171,15 @@
             <div class="contacts-footer">
                 <h5>Contacto</h5>
                 <div class="content-footer">
-                    <img src="public/assets/icon/icons8_facebook_208px_2.png" alt="">
+                    <img src="public/assets/icon/icons8_facebook_208px_2.png" alt="Facebook">
                     <p>facebook/piteu</p>
                 </div>
                 <div class="content-footer">
-                    <img src="public/assets/icon/icons8_instagram_208px.png" alt="">
+                    <img src="public/assets/icon/icons8_instagram_208px.png" alt="Instagram">
                     <p>@piteu</p>
                 </div>
                 <div class="content-footer">
-                    <img src="public/assets/icon/icons8_twitter_208px.png" alt="">
+                    <img src="public/assets/icon/icons8_twitter_208px.png" alt="Twitter">
                     <p>@piteu</p>
                 </div>
             </div>
@@ -201,23 +195,23 @@
                 </nav>
             </div>
             <div class="desc">
-                <div class="desc-tilte">
+                <div class="desc-title">
                     <h5><span>P</span>itéu</h5>
-                    <img src="public/assets/icon/lineFooter.png" alt="">
+                    <img src="public/assets/icon/lineFooter.png" alt="Line">
                 </div>
-                <p> Sabores autênticos da culinária angolana. Reserve sua mesa e experimente uma jornada <br>
-                    gastronômica inesquecível. Atendimento excepcional em um ambiente acolhedor
+                <p>Sabores autênticos da culinária angolana. Reserve sua mesa e experimente uma jornada <br>
+                   gastronômica inesquecível. Atendimento excepcional em um ambiente acolhedor.
                 </p>
             </div>
         </div>
     </footer>
+    <script>
+        window.addEventListener('load', function() {
+            setTimeout(function() {
+                document.querySelector('.loader-container').style.display = 'none';
+            }, 500); 
+        });
+    </script>
+    <script src="public/js/index.js"></script>
 </body>
-<script>
-    window.addEventListener('load', function() {
-      setTimeout(function() {
-        document.querySelector('.loader-container').style.display = 'none';
-      }, 500); 
-    });
-</script>
-<script src="public/js/index.js"></script>
 </html>
